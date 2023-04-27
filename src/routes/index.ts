fa2658c71baa992d87bcfcd878818eb00b40bebe
eduardscaueru <edi_scaueru@yahoo.com>
2023-04-27 18:35:21 +0300

    Deleted comments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,20 +1,7 @@
 import {Router} from "express";
 import db from "./db.routes";
-// import user from "./user.routes";
-// import feed from "./feed.router";
-// import group from "./group.routes";
-// import post from "./post.routes";
-// import chat from "./chat.routes";
-// import comment from "./comment.routes"
 
 const routes = Router();
 
 routes.use("/db", db);
-// routes.use("/user", user);
-// routes.use("/feed", feed);
-// routes.use("/group", group);
-// routes.use("/post", post);
-// routes.use("/chat", chat);
-// routes.use("/comment", comment);
-
 export default routes;
